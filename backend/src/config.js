4b88da5d0f0493ab68a4f472a63c8cf38e7d802a
Yader <yaderbarahona180@gmail.com>
2025-01-19 14:51:39 -0600

    add backend folder

---
+++ backend/src/config.js
@@ -0,0 +1,11 @@
+import { config } from "dotenv";
+config();
+
+export const PORT = "secret123";
+export const DB_HOST = "secret123";
+export const DB_DATABASE = "secret123";
+export const DB_PORT = "secret123";
+export const URL = process.env.URL;
+
+export const TOKEN_SECRET = "secret123";
+export const JWT_EXPIRES = "secret123";
--- backend/src/db.js
+++ backend/src/db.js
@@ -1,8 +1,10 @@
 import mongoose from "mongoose";
+import { URL } from "./config.js";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost/merndb");
+    // await mongoose.connect("mongodb://localhost/merndb");
+    await mongoose.connect(URL);
 
     console.log("DB is connected");
   } catch (error) {
