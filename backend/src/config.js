7da912460a8a2a68697d7af7b6b738796b9f84a1
Yader <yaderbarahona180@gmail.com>
2025-01-19 15:08:17 -0600

    configure env backend and frontend

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,11 +1,11 @@
 import { config } from "dotenv";
 config();
 
-export const PORT = "secret123";
-export const DB_HOST = "secret123";
-export const DB_DATABASE = "secret123";
-export const DB_PORT = "secret123";
-export const URL = process.env.URL;
+export const PORT = process.env.PORT || 4000;
+export const DB_HOST = process.env.DB_HOST || "localhost";
+export const DB_DATABASE = process.env.DB_DATABASE || "merndb";
+export const DB_PORT = process.env.DB_PORT || 27017;
+export const URL = process.env.URL || "mongodb://localhost/merndb";
 
-export const TOKEN_SECRET = "secret123";
-export const JWT_EXPIRES = "secret123";
+export const TOKEN_SECRET = process.env.TOKEN_SECRET || "secret123";
+export const JWT_EXPIRES = process.env.JWT_EXPIRES || "1d";
